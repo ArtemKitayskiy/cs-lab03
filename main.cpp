b1d294f1933e1ab721a650e3b5a50b5b55f3585f
lab03 (KitayskiyAS) <lab03@example.com>
2020-05-08 12:27:22 +0300

    code: упорядочение передачи данных по программе 3

--- main.cpp
+++ main.cpp
@@ -81,16 +81,25 @@ void show_histogram_text(const vector<size_t>& bins)
 }
 
 Input
-read_input(istream& in)
+read_input(istream& in, bool prompt)
 {
     Input data;
-    cerr << "Enter number count: ";
     size_t number_count;
+    if(prompt==true)
+    {
+        cerr << "Enter number count: ";
+    }
     cin >> number_count;
-    cerr << "Enter numbers: ";
+    if(prompt==true)
+    {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
-    cerr << "Enter column count: ";
     size_t bin_count;
+    if (prompt==true)
+    {
+        cerr << "Enter column count: ";
+    }
     cin >>bin_count;
     data.bin_count=bin_count;
     return data;
@@ -98,7 +107,7 @@ read_input(istream& in)
 
 int main()
 {
-    const auto Input = read_input(cin);
+    const auto Input = read_input(cin, true);
     const auto bins =make_histogram(Input);
     show_histogram_svg(bins);
     return 0;
