893046a19c14e734c79bf7ea1b342ef3e5de9db9
lab03 (KitayskiyAS) <lab03@example.com>
2020-05-08 15:21:19 +0300

    code: загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -110,12 +110,12 @@ int main(int argc, char* argv[])
 {
     if(argc>1)
     {
-        cout << "argc=" << argc << endl;
-        for(int i = 0; i < argc; i++)
-        {
-            cout<< "argv["<< i <<"]=" << argv[i] << endl;
-        }
-        return 0;
+         CURL* curl =curl_easy_init();
+         CURLcode res;
+         curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+         res = curl_easy_perform(curl);
+         curl_easy_cleanup(curl);
+         return 0;
     }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto Input = read_input(cin, true);
