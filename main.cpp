0e65ad2bda1efc6df487623e83d26c00e49f2f81
lab03 (KitayskiyAS) <lab03@example.com>
2020-05-08 11:25:44 +0300

    code: упорядочение передачи данных по программе 1

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+struct Input
+{
+    vector<double> numbers;
+    size_t bin_count;
+}
+;
+
 vector<double> input_numbers(istream&in, size_t count)
 {
     vector<double> result(count);
@@ -73,20 +80,26 @@ void show_histogram_text(const vector<size_t>& bins)
     }
 }
 
-int main()
+Input
+read_input(istream& in)
 {
-    size_t number_count;
+    Input data;
     cerr << "Enter number count: ";
+    size_t number_count;
     cin >> number_count;
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(cin, number_count);
-    size_t bin_count;
+    data.numbers = input_numbers(in, number_count);
     cerr << "Enter column count: ";
-    cin >> bin_count;
+    size_t bin_count;
+    cin >>data.bin_count;
+    return data;
+}
 
-    const auto bins = make_histogram(numbers, bin_count);
-
+int main()
+{
+    Input data;
+    data=read_input(cin);
+    const auto bins =make_histogram(data.numbers, data.bin_count);
     show_histogram_svg(bins);
-
     return 0;
 }
